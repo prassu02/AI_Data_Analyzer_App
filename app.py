b3fa8705b74e862613d4c1f63141f36d4070573b
Prasanna kumar <142135618+prassu02@users.noreply.github.com>
2026-03-07 16:00:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
